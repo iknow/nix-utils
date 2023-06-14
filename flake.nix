f1266ecd974eff22e012e8e96d9586c37f67e077
Bearice Ren <bearice@icybear.net>
2023-06-14 11:02:42 +0900

    enable multi-arch build

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
-  outputs = { self, nixpkgs }: {
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (system: 
+    {
     lib = {
       sources = import ./sources.nix { inherit (nixpkgs) lib; };
 
       # this does lock us to a different nixpkgs version for the docker build
       # steps but it ensures the API is what we expect
       docker = import ./docker.nix {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        pkgs = import nixpkgs {inherit system;};
       };
     };
-    utils.x86_64-linux.oci = (import nixpkgs { system = "x86_64-linux"; }).callPackage ./oci {};
-  };
+    utils.oci = (import nixpkgs { inherit system; }).callPackage ./oci {};
+  });
 }
