22e931f0316e766807988b541269333190d8a69b
Bearice Ren <bearice@icybear.net>
2023-06-16 16:00:35 +0900

    simplify things

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }: 
-  flake-utils.lib.eachDefaultSystem (system: 
-    {
-    lib = {
-      sources = import ./sources.nix { inherit (nixpkgs) lib; };
-
-      # this does lock us to a different nixpkgs version for the docker build
-      # steps but it ensures the API is what we expect
-      docker = import ./docker.nix {
-        pkgs = import nixpkgs {inherit system;};
-      };
-    };
-    utils.oci = (import nixpkgs { inherit system; }).callPackage ./oci {};
-  });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) callPackage;
+      in
+      {
+        lib = {
+          sources = callPackage ./sources.nix {};
+          # this does lock us to a different nixpkgs version for the docker build
+          # steps but it ensures the API is what we expect
+          docker = callPackage ./docker.nix {};
+        };
+        utils.oci = callPackage ./oci {};
+      }
+    );
 }
